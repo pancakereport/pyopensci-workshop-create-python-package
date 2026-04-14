6f03e5bed635991d47f27a14cc66782375be224a
Silas Santini <70163606+pancakereport@users.noreply.github.com>
2026-04-14 02:21:23 +0000

    initial package updates

---
+++ src/pyospackage_pancakereport/stars.py
@@ -0,0 +1,11 @@
+"""
+A module about stars.
+
+Inspo from "Airplanes" by B.o.B. and and Hayley Williams
+"""
+
+def airplanes():
+    print("airplanes in the night sky")
+
+def shooting_stars():
+    print("i could really use a wish right now")
