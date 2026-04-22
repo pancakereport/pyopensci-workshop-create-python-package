db92ed086bc2ffee16d7265883f9740a4432a06e
Silas Santini <70163606+pancakereport@users.noreply.github.com>
2026-04-22 01:34:17 +0000

    add more functions

--- src/pyospackage_pancakereport/stars.py
+++ src/pyospackage_pancakereport/stars.py
@@ -1,9 +1,27 @@
 """
-Inspo from "Airplanes" by B.o.B. and Hayley Williams
+This module is based on songs about airplanes, aeroplanes, 
+and jet planes.
 """
 
 def airplanes():
-    print("airplanes in the night sky")
+    print("Airplanes in the night sky...")
 
 def shooting_stars():
-    print("i could really use a wish right now")
+    print("I could really use a wish right now.")
+
+def aeroplane(artist):
+    if artist.lower() == "frou frou":
+        print("Just flying with my aeroplane.")
+    elif artist.lower() == "red hot chili peppers":
+        print("And music is my aeroplane.")
+    elif artist.lower() == "bjork":
+        print("I'm taking an aeroplane across the world to follow my heart.")
+    else:
+        print("I didn't know that artist has an aeroplane song.")
+
+def jet_plane_leaving():
+    print("I'm leaving on a jet plane")
+    print("Don't know when I'll be back again")
+
+def jet_plane_ride():
+    print("Gonna take her for a ride on a big jet plane.")
